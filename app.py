9a22ffe643b02d822011db380001b492b9b9664c
adityawdubey <adityawdubey@gmail.com>
2024-07-03 21:03:21 +0530

    Integrated dotenv to load environment variables from .env file

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ import aws_cdk as cdk
 
 from stacks.main_stack import main_stack
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Retrieve parameters from environment variables or other sources
 email_subscription_endpoint = os.getenv('EMAIL_SUBSCRIPTION_ENDPOINT')
 file_upload_bucket=os.getenv('FILE_UPLOAD_BUCKET')
 
 app = cdk.App()
 main_stack(app, "S3FileUploadNotification",
+        # account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        # region=os.environ["CDK_DEFAULT_REGION"],
         email_subscription_endpoint=email_subscription_endpoint,
         file_upload_bucket=file_upload_bucket
     )
 
 app.synth()
+
+
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,23 +3,13 @@
 # Ensure the script stops on errors
 set -e
 
-# Path to the environment parameters file
-ENV_FILE="parameters/dev-parameters.json"
-
-# Check if the parameters file exists
-if [ -f "$ENV_FILE" ]; then
-    # Export parameters as environment variables
-    export EMAIL_SUBSCRIPTION_ENDPOINT=$(jq -r '.[] | select(.ParameterKey=="EmailSubscriptionEndpoint") | .ParameterValue' "$ENV_FILE")
-    export FILE_UPLOAD_BUCKET=$(jq -r '.[] | select(.ParameterKey=="FileUploadDestinationBucket") | .ParameterValue' "$ENV_FILE")
-else
-    echo "Parameters file $ENV_FILE not found!"
-    exit 1
-fi
-
 # Bootstrap CDK environment (if not already done)
 cdk bootstrap
 
 # Synthesize CloudFormation templates
 cdk synth
 
-cdk deploy --all 
+# Deploy the stack
+echo "Deploying the stack..."
+cdk deploy --all
+echo "Deployment process completed."
